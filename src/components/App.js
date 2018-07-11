0f980fa5db43edd7a0ecb8d0e442e54d7f630690
rohyadav <rohityadav.techie@gmail.com>
2018-07-12 00:44:10 +0530

    :sparkles: refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <React.Fragment>
         <h1>Wow..! React is ON</h1>
-      </Fragment>
+      </React.Fragment>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "/dist")
+    // path: path.join(__dirname, "/dist")
+    path: path.resolve("dist")
     // filename: "bundle.js" // filename value is the name of the minified HTML that will be generated in the dist folder.
   },
   module: {
@@ -18,17 +20,22 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        use: [
-          "style-loader", // load the style into the DOM
-          "css-loader", // take the css & convert to js
-          "sass-loader" // load the sass file & also have node-sass which converts scss -> css file
-        ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader", // load the style into the DOM
+          use: "css-loader!sass-loader" // load the sass file & also have node-sass which converts scss -> css file
+        })
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html" // Used template key for looking HTML file.
-    })
-  ]
+    }),
+    new ExtractTextPlugin("style.css")
+  ],
+  devtool: "cheap-module-eval-source-map",
+  // using this devtool get the exeact location of error occured.
+  devServer: {
+    historyApiFallback: true
+  }
 };
